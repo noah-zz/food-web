428ff11554e1c00d1f9b7867dbb017de4ec404be
Nguyen Mau Nam <nguyenmaunamit@gmail.com>
2024-07-09 14:13:20 +0700

    "verison 1"

---
+++ components/footer.php
@@ -0,0 +1,42 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>Số điện thoại liên hệ:</h3>
+         <a href="tel:#">123-456-7890</a>
+         <a href="tel:#">111-222-3333</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Giờ hoạt động:</h3>
+         <p>Mở cửa: 7H </p>
+         <p>Đóng cửa: 10H</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>Địa chỉ:</h3>
+         <a href="#">Binh Thạnh</a>
+         <a href="#">HCM</a>
+      </div>
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Email:</h3>
+         <a href="mailto:shopfood@gmail.com">shopfood@gmail.com</a>
+         <a href="mailto:shopfood@gmail.com">shopfood@gmail.com</a>
+      </div>
+
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Noahz</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
